21e6250c8788145e2cc245869827f81484ca8eb3
pachkapelmeney <nikitaplanes@gmail.com>
2025-10-27 22:39:56 +0300

    init

---
+++ programs/vault/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("DPPxbB76ZJ1USzniBZJx8Qo8Kgn1wvcXw8xBNgseeXhn");
+
+#[program]
+pub mod vault {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
